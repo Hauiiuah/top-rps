5b18521d6bd48a11ce12c88e212e7f5f6450d54a
Jan Haubrock <jan.haubrock@live.com>
2022-06-28 13:10:50 +0200

    create ui for rps

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 
+let playerScore=0
+let computerScore=0
 
+const buttons = document.querySelectorAll('.buttonRow button')
+const resetButton = document.getElementById('resetButton')
+const resultContainer = document.getElementById('results')
+const scoreBoard = document.getElementById('scores')
 
 const computerPlay =() => {
     const SELECTIONS=['rock','paper','scissors']
@@ -9,46 +15,88 @@ const computerPlay =() => {
 }
 
 const playRound=(computerSelection, playerSelection) => {
-    console.log('Computer selection :',computerSelection)
-    console.log('Player selection: ',playerSelection)
     if(computerSelection== playerSelection)
     {
-        return 0
+        return {draw: true}
     }
     
     if((computerSelection==='rock' && playerSelection==='paper') ||
     (computerSelection==='paper' && playerSelection==='scissors')||
     (computerSelection==='scissors'&& playerSelection==='rock'))
     {
-        return 1
+        return {winner: 'Player',winnerSelection: playerSelection, looserSelection: computerSelection}
     }
-    return -1
+    return {winner: 'Computer',winnerSelection:computerSelection,looserSelection: playerSelection}
 
 }
 
-const playerPlay=()=> {
-    const input =prompt('Please enter "Rock", "Paper" or "Scissors" ')
-    const playerSelection=input.toLowerCase()
-    return playerSelection
+const playerPlay=(e)=> {
+    
+    const playerSelection = e.target.innerText.toLowerCase()
+    const result = playRound(computerPlay(),playerSelection)
+    updateScore(result)
+    printResult(result)
 }
 
+const printResult = (result) => {
 
-
-const evaluateWinner = (score) => {
-    return score < 0 ? 'Computer wins' : score > 0 ? 'Player wins' : 'Draw'
-}
-
-const game=() =>{
-    let score=0
-    for(let i=0;i<5;i++)
+    const message = document.createElement('p')
+    if(result.draw)
     {
-        const playerSelection = playerPlay()
-        const computerSelection = computerPlay()
-        score += playRound(computerSelection,playerSelection)
+        message.innerText="DRAW"
+        message.classList.add('draw')
     }
+    else
+    {
+        message.innerText=`${result.winner} won with ${result.winnerSelection} against ${result.looserSelection}`
+        result.winner==='Player' ? message.classList.add('win') : message.classList.add('loose')
 
-    console.log(evaluateWinner(score))
- 
+    }
+    resultContainer.appendChild(message)
 }
 
-game()
+const updateScore= (result) => {
+
+    scoreBoard.innerHTML=""
+    const scores = document.createElement('p')
+    if(result.winner==='Computer')
+    {
+        computerScore++
+    }
+    else if(result.winner==='Player')
+    {
+        playerScore++
+    }
+    scores.innerText=`Player: ${playerScore} :: Computer: ${computerScore}`
+    if(playerScore===5)
+    {
+        scores.innerText='Player won!'
+        scores.classList.add('win')
+        hideButtons(true)
+    }
+    if(computerScore===5)
+    {
+        scores.innerText='Computer won!'
+        scores.classList.add('loose')
+        hideButtons(true)
+    }
+   
+    scoreBoard.appendChild(scores)
+}
+
+const hideButtons=(hide) => {
+    hide? buttons.forEach(button => button.classList.add('hidden')): buttons.forEach(button => button.classList.remove('hidden'))
+}
+const resetGame = ()=> {
+    playerScore=0
+    computerScore=0
+    resultContainer.innerHTML=''
+    hideButtons(false)
+    updateScore({})
+}
+
+
+
+buttons.forEach(button => button.addEventListener('click',playerPlay))
+resetButton.addEventListener('click',resetGame)
+updateScore({})
